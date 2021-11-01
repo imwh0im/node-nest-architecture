c8deb88a810ffbda00b68df4c072a08c24a265bb
imwh0im <soc06103@gmail.com>
2021-11-02 01:06:32 +0900

    Add Authorization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userSerivce: UsersService) {}
+
+  public validateUser(username: string, password: string) {
+    const user = this.userSerivce.findOne(username, password);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../interface/users';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      userId: 1,
+      username: 'henry',
+      password: 'qwer1234',
+    },
+    {
+      userId: 2,
+      username: 'hyeok jun',
+      password: 'abcd1234',
+    },
+  ]
+
+  public findAll(): User[] {
+    return this.users;
+  }
+
+  public findOne(username: string, password: string): User | undefined {
+    return this.users.find((user) => {
+      return user.username === username && user.password === password;
+    });
+  }
+}
